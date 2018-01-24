5d1d496083b01fa01b54ecd3ba84cba65c1b8ddc
Etwaroo Ihsan <etwarooi@teach.cs.toronto.edu>
2018-01-24 12:08:20 -0500

    script for create_random_file

---
+++ create_random_file_script.py
@@ -0,0 +1,21 @@
+import subprocess
+import os
+
+crf_path = (os.path.realpath('create_random_file'))
+dummy_file = "ihsan_file"
+file_size = 8000000
+num_runs = 10
+block_sizes = [100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 3000000]
+for bs in block_sizes:
+    runs = []
+    for i in range(num_runs):
+        proc = subprocess.Popen([crf_path, dummy_file, str(file_size), str(bs)], stdout=subprocess.PIPE)
+        stdout = proc.communicate([0])
+        bps = float(stdout[0].split(":")[1].split(" ")[1])
+        runs.append(bps)
+
+    avg = sum(runs)/num_runs
+    print("average for block size ({}) : {} bps".format(
+        bs,
+        avg
+    ))
