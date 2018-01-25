f61355449ed8c1aebdf9c95db258ed2b60009305
Etwaroo Ihsan <etwarooi@teach.cs.toronto.edu>
2018-01-25 11:23:48 -0500

    added tar and print statements

--- get_histogram_script.py
+++ get_histogram_script.py
@@ -6,6 +6,7 @@ dummy_file = "ihsan_file"
 num_runs = 100
 block_sizes = [100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 3000000]
 block_avgs = []
+print ("Running get_histogram on 10 diff block sizes between 100B and 3MB")
 for bs in block_sizes:
     runs = []
     for i in range(num_runs):
