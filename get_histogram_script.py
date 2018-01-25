33a3079e55b44b1b25d4cd148e473fde0dc72f88
Etwaroo Ihsan <etwarooi@teach.cs.toronto.edu>
2018-01-24 20:42:46 -0500

    added histogram script

---
+++ get_histogram_script.py
@@ -0,0 +1,27 @@
+import subprocess
+import os
+
+crf_path = (os.path.realpath('get_histogram'))
+dummy_file = "ihsan_file"
+num_runs = 100
+block_sizes = [100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 3000000]
+block_avgs = []
+for bs in block_sizes:
+    runs = []
+    for i in range(num_runs):
+        proc = subprocess.Popen(
+            [crf_path, dummy_file, str(bs)],
+            stdout=subprocess.PIPE
+        )
+        stdout = proc.communicate([0])
+        bps = float(stdout[0].split("DATA RATE")[1].split(" ")[1])
+        if (float('inf') != bps):
+            runs.append(bps)
+
+    avg = sum(runs)/len(runs)
+    block_avgs.append(avg)
+    print("average for block size ({}) : {} bps".format(
+        bs,
+        avg
+    ))
+    print(runs)
